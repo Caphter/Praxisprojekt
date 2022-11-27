7ebeb7a01cb6a62012019cb55b7dda61b820ddf2
Caphter <60566659+Caphter@users.noreply.github.com>
2022-11-27 18:53:14 +0100

    Zwischenspeicher Positions-Tracking Script

--- Assets/[Scripts]/HapticDeviceManager.cs
+++ Assets/[Scripts]/HapticDeviceManager.cs
@@ -7,8 +7,14 @@ using Valve.VR;
 public class HapticDeviceManager : MonoBehaviour
 {
     /* 
-    - Check kann beim Starten/Resetten der Szene gemacht werden oder während der Laufzeit (Wenn während der Laufzeit muss genau gemacht werden
-    -> Timer einbauen der in bestimmten Abstand abfragt, ob die Wechselkriterien erfüllt werden
+    - HIERARCHIE: Controller > Sense Gloves > Hand Tracking
+    -> Fall einbauen, dass Controller mit Sense-Gloves benutzt werden
+
+
+    - UI für 2 Wechsel Optionen -> Manuell o. Automatisch
+    -> Automatisch: Wenn sie die Koordinaten eines Gerätes ändern wird eine Funktion aufgerufen, die ein weiteres Ändern checkt und nach Schwellenwertüberschreitung das Gerät wechselt
+    -> Manuell: Es gibt für das gerade aktive Gerät 2 Buttons die jeweils ein- und ausgeblendet werden für die jeweils anderen Geräte
+    -> UI in Reichweite platzieren, dafür einen UI-Schließ- und Aufrufbutton einbauen
 
     - Hand Tracking ist Baseline
     -> Individuelle Geräte-UI nach ein paar Sekunden erst einblenden, damit das Programm die Möglichkeit hat das aktive Gerät zu identifizieren.
@@ -26,20 +32,80 @@ public class HapticDeviceManager : MonoBehaviour
     private float timerCheckActiveHapticDevice = 0;
 
     // Devices
+    [Header("Haptic Devices:")]
     public GameObject senseGlovesDevice;
     public GameObject controllerDevice;
     public GameObject handTrackingDevice;
 
+    // Zu trackende Positionen
+    [Header("Device Positions:")]
+    public GameObject goRightController;
+    public GameObject goLeftController;
+    public GameObject goRightTracker;
+    public GameObject goLeftTracker;
+
+    private Vector3 posRightController;
+    private Vector3 posLeftController;
+    private Vector3 posRightTracker;
+    private Vector3 posLeftTracker;
+
+    private Vector3 lastPosRightController;
+    private Vector3 lastPosLeftController;
+    private Vector3 lastPosRightTracker;
+    private Vector3 lastPosLeftTracker;
+
+    // Positions-Veränderung Berechnungsvariablen
+    //private float offsetRotationTrackerLeft;
+    //private float offsetRotationTrackerRight;
+    //private float offsetRotationControllerLeft;
+    //private float offsetRotationControllerRight;
+
+    public float offsetPositionTrackerLeft;
+    public float offsetPositionTrackerRight;
+    public float offsetPositionControllerLeft;
+    public float offsetPositionControllerRight;
+
+    private float thresholdTracker;
+    private float threshholdController;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        // Device Hierarchie Start
+        //senseGlovesDevice.SetActive(false);
+        //controllerDevice.SetActive(false);
+        //handTrackingDevice.SetActive(false);
+
+        StartCoroutine(InitializePositions());
+
+        Debug.Log("Zuweisung");
         
+    }
+
+    IEnumerator InitializePositions()
+    {
+        yield return new WaitForSeconds(2f);
+
+        posRightController = goRightController.transform.localPosition;
+        posLeftController = goLeftController.transform.localPosition;
+        posLeftTracker = goLeftTracker.transform.localPosition;
+        posRightTracker = goRightTracker.transform.localPosition;
+
+        lastPosLeftController = posLeftController;
+        lastPosLeftTracker = posLeftTracker;
+        lastPosRightController = posRightController;
+        lastPosRightTracker = posRightTracker;
     }
 
     // Update is called once per frame
     void Update()
     {
+        posRightController = goRightController.transform.localPosition;
+        posLeftController = goLeftController.transform.localPosition;
+        posLeftTracker = goLeftTracker.transform.localPosition;
+        posRightTracker = goRightTracker.transform.localPosition;
+
+
         // erhöht den Timer und ruft alle 3 Sekunden die Funktion zum checken des aktiven haptischen Gerätes auf
         timerCheckActiveHapticDevice += Time.deltaTime;
 
@@ -47,11 +113,33 @@ public class HapticDeviceManager : MonoBehaviour
         {
             checkActiveHapticDevice();
         }
+
+        
+        // Checkt, ob es eine Änderung in der Positionen der Geräte gab zum letzten Frame
+        if(lastPosLeftController != posLeftController || lastPosRightController != posRightController)
+        {
+            offsetPositionControllerLeft += (float)System.Math.Round(Mathf.Abs(posLeftController.x - lastPosLeftController.x),2) + (float)System.Math.Round(Mathf.Abs(posLeftController.y - lastPosLeftController.y),2) + (float)System.Math.Round(Mathf.Abs(posLeftController.z - lastPosLeftController.z),2);
+            offsetPositionControllerRight += Mathf.Abs(posRightController.x - lastPosRightController.x) + Mathf.Abs(posRightController.y - lastPosRightController.y) + Mathf.Abs(posRightController.z - lastPosRightController.z);
+
+            lastPosLeftController = goLeftController.transform.localPosition;
+            lastPosRightController = goRightController.transform.localPosition;
+        }
+
+        if (lastPosLeftTracker != posLeftTracker || lastPosRightTracker != posRightTracker)
+        {
+            offsetPositionTrackerLeft += Mathf.Abs(posLeftTracker.x - lastPosLeftTracker.x) + Mathf.Abs(posLeftTracker.y - lastPosLeftTracker.y) + Mathf.Abs(posLeftTracker.z - lastPosLeftTracker.z);
+            offsetPositionTrackerRight += Mathf.Abs(posRightTracker.x - lastPosRightTracker.x) + Mathf.Abs(posRightTracker.y - lastPosRightTracker.y) + Mathf.Abs(posRightTracker.z - lastPosRightTracker.z);
+
+            lastPosLeftTracker = goLeftTracker.transform.localPosition;
+            lastPosRightTracker = goRightTracker.transform.localPosition;
+        }
+
     }
 
     public void checkActiveHapticDevice()
     {
         // initiales zurücksetzen der tracking-Variablen für neue Iteration
+        timerCheckActiveHapticDevice = 0f;
         controllerActivated = false;
         trackerActivated = false;
 
